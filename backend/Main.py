7fa41aafa460407b0fcbc15a36d2d489fbd25256
sandhya <sravanisandhya492@gmail.com>
2026-09-13 22:09:48 +0530

    Allow Vercel frontend in CORS

--- backend/Main.py
+++ backend/Main.py
@@ -20,7 +20,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://mystery-mind-lgcz.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
